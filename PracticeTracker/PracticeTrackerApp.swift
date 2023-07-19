05846294d92923ea8c0c12b9b9437259ecac8c40
Anissa Santos <anissa.n.santos@gmail.com>
2023-07-19 11:37:58 -0500

    Initial Commit

---
+++ PracticeTracker/PracticeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeTrackerApp.swift
+//  PracticeTracker
+//
+//  Created by Scholar on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct PracticeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
